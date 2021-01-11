3eba700f1f50b121947e4230f1437cfc7bfdc3a6
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2021-01-11 11:30:37 -0600

    Created Base Styled Components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Game from "./pages/Game";
 import HighScores from "./pages/HighScores";
 import GameOver from "./pages/GameOver";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import { Container } from "./styled/Container";
+import { Main } from "./styled/Main";
+import Global from "./styled/Global";
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/game" component={Game} />
-        <Route path="/highScores" component={HighScores} />
-        <Route path="/gameOver" component={GameOver} />
-        <Route path="/" component={Home} />
-      </Switch>
+      <Global />
+      <Main>
+        <Container>
+          <Navbar />
+          <Switch>
+            <Route path="/game" component={Game} />
+            <Route path="/highScores" component={HighScores} />
+            <Route path="/gameOver" component={GameOver} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Container>
+      </Main>
     </Router>
   );
 }
