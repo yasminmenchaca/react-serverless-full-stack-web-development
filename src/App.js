2dc87d9af0b04fe839d41aba71ba55b6b9c32eec
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2021-01-11 11:04:30 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Game from "./pages/Game";
 import HighScores from "./pages/HighScores";
 import GameOver from "./pages/GameOver";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route path="/game" component={Game} />
         <Route path="/highScores" component={HighScores} />
