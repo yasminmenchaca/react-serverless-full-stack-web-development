7748e3afa8bc1a902deb3dcd43dabe6713eaeba0
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2021-01-11 10:49:34 -0600

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Game from "./pages/Game";
+import HighScores from "./pages/HighScores";
+import GameOver from "./pages/GameOver";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/game" component={Game} />
+        <Route path="/highScores" component={HighScores} />
+        <Route path="/gameOver" component={GameOver} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
