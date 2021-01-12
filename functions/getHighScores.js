35936fa83a09ff44da092ccf526385bcd9bbaa86
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2021-01-12 15:39:41 -0600

    serverless functions to retrieve high scores from airtable

---
+++ functions/getHighScores.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const Airtable = require("airtable");
+
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_BASE_ID
+);
+
+const table = base(process.env.AIRTABLE_TABLE_NAME);
+
+exports.handler = async (event) => {
+  try {
+    const records = await table.select().firstPage();
+    const formattedRecords = records.map((record) => ({
+      id: record.id,
+      fields: record.fields,
+    }));
+    return {
+      statusCode: 200,
+      body: JSON.stringify(formattedRecords),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: "Failed to query records in Airtable" }),
+    };
+  }
+};
