bd393cd18702f7cdd6c8132f205170d0e2c1e6bf
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2021-01-12 16:36:16 -0600

    high scores are displaying from airtable api in desc order, styling changes 🙌🏼

--- src/pages/HighScores.js
+++ src/pages/HighScores.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ScoresList, ScoreLI } from "../styled/HighScores";
 
 export default function HighScores() {
+  //display those scores
+  const [highScores, setHighScores] = useState([]);
+
+  //use the fetch API to call the getHighScores function
+  useEffect(() => {
+    console.log("getting high scores");
+
+    const loadHighScores = async () => {
+      try {
+        const res = await fetch("/.netlify/functions/getHighScores");
+        const scores = await res.json();
+        setHighScores(scores);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    loadHighScores();
+  }, []);
+
   return (
     <div>
-      <h1>HighScores</h1>
+      <h1>High Scores</h1>
+      <ScoresList>
+        {highScores.map((score) => (
+          <ScoreLI key={score.id}>
+            {score.fields.name} - {score.fields.score}
+          </ScoreLI>
+        ))}
+      </ScoresList>
     </div>
   );
 }
