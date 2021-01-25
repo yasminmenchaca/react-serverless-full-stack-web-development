f102762e2c1a699ceb37ad26e279abe609bceaf9
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2021-01-25 14:12:08 -0600

    Retrieve and Parse Access Token

--- functions/saveHighScore.js
+++ functions/saveHighScore.js
@@ -1,6 +1,14 @@
 const { table, getHighScores } = require("../functions/utils/airtable");
+const { getAccessTokenFromHeaders } = require("../functions/utils/auth.js");
 
 exports.handler = async (event) => {
+  const token = getAccessTokenFromHeaders(event.headers);
+  if (!token) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ err: "User is not logged in" }),
+    };
+  }
   if (event.httpMethod !== "POST") {
     return {
       statusCode: 405,
