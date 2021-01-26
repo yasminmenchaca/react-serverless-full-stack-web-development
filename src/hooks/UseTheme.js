33757fb13fa3e1becd9dda19ec7e55142f4a67ae
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2021-01-26 14:06:26 -0600

    Theme Choice in Local Storage

--- src/hooks/UseTheme.js
+++ src/hooks/UseTheme.js
@@ -1,12 +1,20 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { useEffect, useState } from "react";
 export default () => {
   const [theme, setTheme] = useState("light");
 
+  useEffect(() => {
+    const localStorageTheme = window.localStorage.getItem("theme");
+    setTheme(localStorageTheme || "light");
+  }, []);
+
   const toggleTheme = () => {
     if (theme === "light") {
       setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
     } else {
       setTheme("light");
+      window.localStorage.setItem("theme", "light");
     }
   };
 
